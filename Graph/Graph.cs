f8986f4cecb46cb97b3290db43d413ade0f28f11
rampanellijader <jader.rampa@gmail.com>
2021-07-13 00:36:21 -0300

    implementando testes de rotas

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
 
 namespace Graph
 {
@@ -10,14 +12,37 @@ namespace Graph
 
     public class Graph<T> : IGraph<T>
     {
+        readonly Dictionary<T, IEnumerable<T>> dict;
         public Graph(IEnumerable<ILink<T>> links)
         {
-
+            dict = links.GroupBy(l => l.Source)
+                         .ToDictionary(k => k.Key,
+                                       k => k.Select(l => l.Target));
         }
 
         public IObservable<IEnumerable<T>> RoutesBetween(T source, T target)
         {
-            throw new NotImplementedException();
+            var routes = new List<IEnumerable<T>>();
+            var queue = new Queue<IEnumerable<T>>();
+            queue.Enqueue(new List<T> { source });
+            while (queue.Any())
+            {
+                var currentRoute = queue.Dequeue();
+                var currentLastNode = currentRoute.Last();
+                if (dict.TryGetValue(currentLastNode, out var targets))
+                {
+                    foreach (var newTarget in targets.Where(t => !currentRoute.Contains(t)))
+                    {
+                        var nextList = new List<T>(currentRoute) { newTarget };
+
+                        if (EqualityComparer<T>.Default.Equals(newTarget, target))
+                            routes.Add(nextList.AsReadOnly());
+                        else
+                            queue.Enqueue(nextList);
+                    }
+                }
+            }
+            return routes.ToObservable();
         }
     }
 }
--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,38 @@ namespace SubtitleTimeshift
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
-            throw new NotImplementedException();
+            using (var reader = new StreamReader(input, encoding, false, bufferSize, leaveOpen))
+            using (var writer = new StreamWriter(output, encoding, bufferSize, leaveOpen))
+            {
+                string line;
+                var arrow = " --> ";
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    var outputLine = line;
+                    var stamps = line.Split(new[] { arrow }, StringSplitOptions.RemoveEmptyEntries);
+                    if (stamps.Length == 2)
+                    {
+                        var start = shiftStamp(stamps[0], timeSpan);
+                        var end = shiftStamp(stamps[1], timeSpan);
+                        outputLine = $"{start}{arrow}{end}";
+                    }
+
+                    await writer.WriteLineAsync(outputLine);
+                }
+            }
+        }
+
+        static string shiftStamp(string stampText, TimeSpan timeSpan)
+        {
+            var nums = stampText.Split(new[] { ':', ',', '.', '|' })
+                                .Select(s => int.Parse(s))
+                                .ToArray();
+
+            var result = new TimeSpan(0, nums[0], nums[1], nums[2], nums[3])
+                          .Add(timeSpan)
+                          .ToString(@"hh\:mm\:ss\.fff");
+
+            return result;
         }
     }
 }
